b90d71cf80b21282980466eaebbf067fdd0d58d0
henrikdavison <henrik.davison@live.co.uk>
2024-11-08 08:20:35 +0100

    Update builder list to contain placeholders

--- src/components/Step3ArmyBuilder/CategoryItem.js
+++ src/components/Step3ArmyBuilder/CategoryItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Box, Typography, IconButton, Paper } from '@mui/material';
-import { Trash } from 'lucide-react'; // Importing the Lucide Trash icon
+import { Trash, Eye, Edit } from 'lucide-react'; // Importing the Lucide Trash icon
 
 function CategoryItem({ name, points, count, onDelete, isLastItem }) {
   return (
+
     <Paper
       elevation={2} // Small elevation to give a subtle shadow
       sx={{
@@ -18,13 +19,30 @@ function CategoryItem({ name, points, count, onDelete, isLastItem }) {
         minHeight: 56,
       }}
     >
-      <Typography variant="unitName">{name}</Typography>
+      {/* Box to stack name and unitSummary vertically */}
+      <Box display="flex" flexDirection="column" alignItems="flex-start">
+        <Typography variant="unitName">{name}</Typography>
+        <Typography
+          variant="caption" // Smaller text for summary
+          color="text.secondary"
+
+        >
+          unitSummary
+        </Typography>
+      </Box>
+      
       <Box display="flex" alignItems="center">
         <Typography variant="pointsValue" mr={1}>
           {points} pts
         </Typography>
         <IconButton size="medium" onClick={onDelete}>
-          <Trash size={18} /> {/* Lucide Trash icon with a set size */}
+          <Trash size={18} /> {/* Trash icon */}
+        </IconButton>
+        <IconButton size="medium" onClick={onDelete}>
+          <Eye size={18} /> {/* Eye icon */}
+        </IconButton>
+        <IconButton size="medium" onClick={onDelete}>
+          <Edit size={18} /> {/* Edit icon */}
         </IconButton>
       </Box>
     </Paper>
